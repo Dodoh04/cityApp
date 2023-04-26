a9a432267a84681b34ee52f712e66feb41945245
Dodoh04 <douglasoliversa3@gamil.com>
2023-04-25 21:52:32 -0300

    Termino da criação dos layouts

---
+++ app/src/main/java/br/com/etecia/cityapp/TetsActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.cityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TetsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_layout);
+    }
+}
