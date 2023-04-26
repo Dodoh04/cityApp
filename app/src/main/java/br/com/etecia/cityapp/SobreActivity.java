368dd558e7e80135a322261347d819cd6ce471db
Dodoh04 <douglasoliversa3@gamil.com>
2023-04-25 22:07:12 -0300

    faltando apenas o tabLayout

--- app/src/main/java/br/com/etecia/cityapp/SobreActivity.java
+++ app/src/main/java/br/com/etecia/cityapp/SobreActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TetsActivity extends AppCompatActivity {
+public class SobreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test_layout);
+        setContentView(R.layout.sobre_layout);
     }
 }
